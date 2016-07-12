28e0296635aa2767fc469abd73d23353eb8b7184
richards0395 <richards0395@hotmail.com>
2016-07-11 19:44:06 -0500

    modificacion de Motherboard
    -se han hecho diversos cambios en el collection y html de Motherboard.

--- collections/Motherboard.js
+++ collections/Motherboard.js
@@ -1,4 +1,43 @@
 Motherboard = new Mongo.Collection('motherboard');
+PuertosPT= new SimpleSchema({
+pt_PS2_teclado:{
+  type: Number,
+  label:"Puerto P/2 para el teclado",
+},
+pt_PS2_mouse:{
+  type: Number,
+  label:"Puerto P/2 para el mouse",
+},
+pt_VGA:{
+  type: Number,
+  label:"Puerto de video VGA",
+},
+pt_DVI:{
+  type: Number,
+  label: "puerto de video DVI",
+},
+pt_HDMI:{
+  type: Number,
+  label: "puerto HDMI",
+},
+pt_LAN:{
+  type: Number,
+  label: "puerto LAN RJ-45",
+},
+pt_USB2:{
+  type: Number,
+  label: "puerto USB 2.0",
+},
+pt_USB3:{
+  type: Number,
+  label: "puerto USB 3.0",
+},
+pt_audio:{
+  type: Number,
+  label: "puerto de audio",
+},
+});
+
 Tarjeta_Madre = new SimpleSchema({
   mb_nombre:{
     type: String,
@@ -33,23 +72,23 @@ Tarjeta_Madre = new SimpleSchema({
     type: String,
     label: "conector de alimentacion principal ATX de la Tarjeta Madre",
   },
-  // este para el ATX ya sea de 1 o 8 pines, 12 V
+  // este para el ATX ya sea de 4 o 8 pines, 12 V
   mb_conATXsec:{
     type: String,
     label: "conector de alimentacion  pines ATX V de la Tarjeta Madre",
   },
-  mb_tPCI_Ex16:{
-    type: String,
-    label: "tipo de conector PCI-Ex16 de la Tarjeta Madre",
-    },
+  //mb_tPCI_Ex16:{
+  //   type: String,
+  //  label: "tipo de conector PCI-Ex16 de la Tarjeta Madre",
+  //  },
     mb_cPCI_Ex16:{
       type: Number,
       label: "Cantidad de ranuras PCI-Ex16 de la Tarjeta Madre",
   },
-    mb_tPCI_E:{
-      type: String,
-      label: "Tipo de conector PCI-E de la Tarjeta Madre",
-  },
+  //  mb_tPCI_E:{
+  //    type: String,
+  //    label: "Tipo de conector PCI-E de la Tarjeta Madre",
+  //},
   mb_cPCI:{
     type: Number,
     label: "Cantidad de ranuras PCI-E de la Tarjeta Madre",
@@ -60,6 +99,12 @@ Tarjeta_Madre = new SimpleSchema({
     label: "precio en la Tarjeta Madre",
     decimal:true,
   },
+
+  puertos:{
+    type:[PuertosPT]
+  },
+
+
   img:{
     type: String,
     autoform: {
