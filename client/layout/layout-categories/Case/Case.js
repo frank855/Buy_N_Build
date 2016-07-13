43242cca4623ccb10dfab417ab79e82ab6d6c363
guadamuz95 <leonardo.guadamuz@utp.ac.pa>
2016-07-13 04:00:05 -0500

    Adivion de .events para ram, tvideo y case, para la adición al carrito
    -Adición de panel informativo en el home.html
    - cambios en archivo ram.js, tvideo.js y case.js para el uso del carrito
    -adision de rutas en routes.js y sidebar.html

--- client/layout/layout-categories/Case/Case.js
+++ client/layout/layout-categories/Case/Case.js
@@ -1,14 +1,17 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Meteor } from 'meteor/meteor';
+
 import './Casetest.html';
+import '../../../layout-components/Slider.html';
+import '../../../layout-components/Slider.js';
 
 Meteor.subscribe('case');
 
 Template.Casetest.created = function () {
  this.pagination = new Meteor.Pagination(Case, {
        sort: {
-           _id: -1
+            precio: -1,
        }
    });
 }
@@ -17,7 +20,24 @@ Template.Casetest.helpers({
    templatePagination: function () {
        return Template.instance().pagination;
    },
+
  documents: function () {
    return Template.instance().pagination.getPage();
  }
+});
+
+// Este evento lo deben añadir en sus js, basado en su template, fijense en donde esta el "form" en el html
+Template.Casetest.events({
+  'submit form'(events) {
+    event.preventDefault();
+    var user_id= Meteor.userId();
+    //Esto lo sacan del html hay un textbox que esta hidden de esta manera accesan al registro que y lo pueden insertar
+    //en la coleccion cart
+    var id= event.target.idp.value;
+    //Todas sus colecciones deben contar con nombre y precio
+    var prod_nom = Case.findOne({_id:id}).nombre;
+    var prod_precio = Case.findOne({_id:id}).precio;
+    Meteor.call('addcart',id,prod_nom,prod_precio,user_id); //Metodo del servidor
+    cart_total(); //Función global definida en lib/functions,js todos los js la pueden llamar
+  }
 });
--- client/layout/layout-categories/Ram/Ram.js
+++ client/layout/layout-categories/Ram/Ram.js
@@ -1,14 +1,17 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Meteor } from 'meteor/meteor';
+
 import './Ramtest.html';
+import '../../../layout-components/Slider.html';
+import '../../../layout-components/Slider.js';
 
 Meteor.subscribe('ram');
 
 Template.Ramtest.created = function () {
  this.pagination = new Meteor.Pagination(Ram, {
        sort: {
-           _id: -1
+           precio: -1,
        }
    });
 }
@@ -20,4 +23,21 @@ Template.Ramtest.helpers({
  documents: function () {
    return Template.instance().pagination.getPage();
  }
+});
+
+
+// Este evento lo deben añadir en sus js, basado en su template, fijense en donde esta el "form" en el html
+Template.Ramtest.events({
+  'submit form'(events) {
+    event.preventDefault();
+    var user_id= Meteor.userId();
+    //Esto lo sacan del html hay un textbox que esta hidden de esta manera accesan al registro que y lo pueden insertar
+    //en la coleccion cart
+    var id= event.target.idp.value;
+    //Todas sus colecciones deben contar con nombre y precio
+    var prod_nom = Ram.findOne({_id:id}).nombre;
+    var prod_precio = Ram.findOne({_id:id}).precio;
+    Meteor.call('addcart',id,prod_nom,prod_precio,user_id); //Metodo del servidor
+    cart_total(); //Función global definida en lib/functions,js todos los js la pueden llamar
+  }
 });
